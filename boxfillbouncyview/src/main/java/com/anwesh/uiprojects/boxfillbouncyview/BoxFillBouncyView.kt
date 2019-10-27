838391494277fefa95fa979f65f61e0cfcc9b335
Anwesh43 <anweshthecool0@gmail.com>
2019-10-27 10:33:32 +0530

    adding constants and libraries

--- boxfillbouncyview/src/main/java/com/anwesh/uiprojects/boxfillbouncyview/BoxFillBouncyView.kt
+++ boxfillbouncyview/src/main/java/com/anwesh/uiprojects/boxfillbouncyview/BoxFillBouncyView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.boxfillbouncyview
 /**
  * Created by anweshmishra on 27/10/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val delay : Long = 30
+val foreColor : Int = Color.parseColor("#0D47A1")
+val backColor : Int = Color.parseColor("#BDBDBD")
