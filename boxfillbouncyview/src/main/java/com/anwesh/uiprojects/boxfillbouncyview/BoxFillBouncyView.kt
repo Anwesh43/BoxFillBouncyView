a08c737f83af2891cbe95d937e9af33c45cecb38
Anwesh43 <anweshthecool0@gmail.com>
2019-10-27 10:51:29 +0530

    adding BoxFillBouncyView class

--- boxfillbouncyview/src/main/java/com/anwesh/uiprojects/boxfillbouncyview/BoxFillBouncyView.kt
+++ boxfillbouncyview/src/main/java/com/anwesh/uiprojects/boxfillbouncyview/BoxFillBouncyView.kt
@@ -65,3 +65,21 @@ fun Canvas.drawBFBNode(i : Int, scale : Float, paint : Paint) {
     drawBoxFillBouncy(scale, size, paint)
     restore()
 }
+
+class BoxFillBouncyView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
