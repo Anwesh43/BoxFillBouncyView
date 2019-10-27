fc64a0b6eeb6f8f1a2f77fe4f7020bc2d4cd538b
Anwesh43 <anweshthecool0@gmail.com>
2019-10-27 10:47:54 +0530

    adding Canvas extension method

--- boxfillbouncyview/src/main/java/com/anwesh/uiprojects/boxfillbouncyview/BoxFillBouncyView.kt
+++ boxfillbouncyview/src/main/java/com/anwesh/uiprojects/boxfillbouncyview/BoxFillBouncyView.kt
@@ -20,8 +20,48 @@ val sizeFactor : Float = 2.9f
 val delay : Long = 30
 val foreColor : Int = Color.parseColor("#0D47A1")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 180f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawLid(scale : Float, size : Float, paint : Paint) {
+    save()
+    rotate(-180f * scale.sinify())
+    drawLine(0f, 0f, size, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawBoxFill(scale : Float, size : Float, paint : Paint) {
+    for (j in 0..1) {
+        save()
+        translate(size * j, size)
+        drawLine(0f, 0f, 0f, -size, paint)
+        if (j == 0) {
+            drawLine(0f, 0f, size, 0f, paint)
+        }
+        restore()
+    }
+    drawRect(RectF(0f, size - size * scale.divideScale(0, 2), size, size), paint)
+}
+
+fun Canvas.drawBoxFillBouncy(scale : Float, size : Float, paint : Paint) {
+    drawBoxFill(scale, size, paint)
+    drawLid(scale, size, paint)
+}
+
+fun Canvas.drawBFBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawBoxFillBouncy(scale, size, paint)
+    restore()
+}
